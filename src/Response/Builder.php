1cc534edc18405689443dbf6f2989fbe49c4631b
Marek Dąbrowski <notyzop@gmail.com>
2023-02-19 19:16:38 +0100

    add response models

---
+++ src/Response/Builder.php
@@ -0,0 +1,36 @@
+<?php
+
+namespace Mar3q\HttpClient\Response;
+
+use CurlHandlel;
+use Mar3q\HttpClient\Util\Utils;
+
+class Builder
+{
+    /**
+     * @param CurlHandlel $curlHandle
+     * @param $result
+     * @return Response
+     */
+    public static function build($curlHandle, $result): Response
+    {
+        $headerSize = curl_getinfo($curlHandle, CURLINFO_HEADER_SIZE);
+        $headerStr = substr($result, 0, $headerSize);
+        $bodyStr = substr($result, $headerSize);
+
+        $headers = [];
+        foreach(Utils::headersToArray($headerStr) as $key => $value) {
+            $headers[$key] = $value;
+        }
+
+        $curlInfo = curl_getinfo($curlHandle);
+
+        $response = new Response(
+            (int)$curlInfo['http_code'],
+            Utils::streamFor($bodyStr),
+            $headers
+        );
+
+        return $response;
+    }
+}
